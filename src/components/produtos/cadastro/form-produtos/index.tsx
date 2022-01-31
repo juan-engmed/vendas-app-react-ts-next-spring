bbef30150dfadbdb36de3e9f8f4b0778a7360db2
Juan Oliveira <jfs.dev.eng@gmail.com>
2022-01-30 22:19:11 -0300

    created component formProduct

---
+++ src/components/produtos/cadastro/form-produtos/index.tsx
@@ -0,0 +1,51 @@
+export const FormProdutos: React.FC = () => {
+
+    return (
+        <div>
+            <div className='field'>
+                <label className='label' htmlFor="inputSku">SKU: *</label>
+                <div className="control">
+                    <input className='input'
+                        id='inputSku'
+                        placeholder='Digite o SKU do Produto' />
+                </div>
+            </div>
+
+            <div className='field'>
+                <label className='label' htmlFor="inputPrice">Preço: *</label>
+                <div className="control">
+                    <input className='input'
+                        id='inputPrice'
+                        placeholder='Digite o Preço do Produto' />
+                </div>
+            </div>
+
+            <div className='field'>
+                <label className='label' htmlFor="inputName">Nome: *</label>
+                <div className="control">
+                    <input className='input'
+                        id='inputName'
+                        placeholder='Digite o Nome do Produto' />
+                </div>
+            </div>
+
+            <div className='field'>
+                <label className='label' htmlFor="inputDescription">Descrição: *</label>
+                <div className="control">
+                    <textarea className='textarea'
+                        id='inputDescription'
+                        placeholder='Digite a Descrição Detalhada do Produto' />
+                </div>
+            </div>
+
+            <div className="field is-grouped">
+                <div className="control">
+                    <button className="button is-link">Salvar</button>
+                </div>
+                <div className="control">
+                    <button className="button is-link">Voltar</button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -1,52 +1,10 @@
 import { Layout } from 'components'
+import { FormProdutos } from './form-produtos'
 
 export const CadastroProdutos: React.FC = () => {
     return (
         <Layout titulo='Cadastro de Produtos'>
-            <div className='field'>
-                <label className='label' htmlFor="inputSku">SKU: *</label>
-                <div className="control">
-                    <input className='input'
-                    id='inputSku'
-                     placeholder='Digite o SKU do Produto'/>
-                </div>
-            </div>
-
-            <div className='field'>
-                <label className='label' htmlFor="inputPrice">Preço: *</label>
-                <div className="control">
-                    <input className='input'
-                    id='inputPrice'
-                     placeholder='Digite o Preço do Produto'/>
-                </div>
-            </div>
-
-            <div className='field'>
-                <label className='label' htmlFor="inputName">Nome: *</label>
-                <div className="control">
-                    <input className='input'
-                    id='inputName'
-                     placeholder='Digite o Nome do Produto'/>
-                </div>
-            </div>
-
-            <div className='field'>
-                <label className='label' htmlFor="inputDescription">Descrição: *</label>
-                <div className="control">
-                    <textarea className='textarea'
-                    id='inputDescription'
-                     placeholder='Digite a Descrição Detalhada do Produto'/>
-                </div>
-            </div>
-
-            <div className="field is-grouped">
-                <div className="control">
-                    <button className="button is-link">Salvar</button>
-                </div>
-                <div className="control">
-                    <button className="button is-link">Voltar</button>
-                </div>
-            </div>
+        <FormProdutos/> 
 
         </Layout>
     )
